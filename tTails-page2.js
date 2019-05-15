d61c3af44438a90d4f7080fa5abf396bc64caa31
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-05-15 14:19:50 -0400

    addedthe functions that make the markers for hotels, parks, petstores, vets on the map

--- tTails-page2.js
+++ tTails-page2.js
@@ -1,5 +1,8 @@
 "use strict"
-const information = 
+let hotelInfo=`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=hotels-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let parksInfo=`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let storeInfo=`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let vetInfo=`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
 function get(url) {
     return fetch(url)
@@ -51,9 +54,36 @@ function markPetStores(object){
     });
 }
 
+function markVets(object){
+    object.forEach(vet => {
+        const lat = object.result.geometry.location.lat;
+        const lng = object.result.geometry.location.lng;
+
+        const marker = new google.maps.Marker({
+            position:{lat, lng},
+            map: map
+        });
+    });
+}
+
 function updateMap() {
-    get(information)
+    get(hotelInfo)
     .then(function(response){
-             
+        markHotels(response);
     }); 
+
+    get(parksInfo)
+    .then(function(response){
+        markParks(response);
+    })
+
+    get(storeInfo)
+    .then(function(response){
+        markPetStores(response);
+    })
+
+    get(vetInfo)
+    .then(function(response){
+        markVets(response);
+    })
 }
