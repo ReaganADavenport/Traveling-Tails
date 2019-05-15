b9637d8f502d93345e747572db903aff0eab6e4a
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-05-15 14:00:29 -0400

    made function for markParks and markPetStores

--- tTails-page2.js
+++ tTails-page2.js
@@ -26,6 +26,31 @@ function markHotels(object){
     });
 }
 
+
+function markParks(object){
+    object.forEach(park => {
+        const lat = object.result.geometry.location.lat;
+        const lng = object.result.geometry.location.lng;
+
+        const marker = new google.maps.Marker({
+            position:{lat, lng},
+            map: map
+        });
+    });
+}
+
+function markPetStores(object){
+    object.forEach(store => {
+        const lat = object.result.geometry.location.lat;
+        const lng = object.result.geometry.location.lng;
+
+        const marker = new google.maps.Marker({
+            position:{lat, lng},
+            map: map
+        });
+    });
+}
+
 function updateMap() {
     get(information)
     .then(function(response){
