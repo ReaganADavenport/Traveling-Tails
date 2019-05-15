6a213857aeba6fb975f8fb5516ece7dbc2331131
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-15 14:19:35 -0400

    Added Function

--- travelingTails.js
+++ travelingTails.js
@@ -1,22 +1,25 @@
 "use strict"
+import 'tTails-page2.js';
 
-// let hotelInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=hotels-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let hotelInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=hotels-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-// let parksInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let parksInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-// let storeInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let storeInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-// let vetInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+let vetInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
 let firstButton =document.querySelector('#first-page-button');
+let locationInput = document.querySelector('#search-bar');
+
 
 function visitPage(){
     window.location='tTails-page2.html';
 }
 
 firstButton.addEventListener('click', function(){
-    console.log('hi');
     visitPage();
+    markHotels(response);
 });
 
 
@@ -59,11 +62,6 @@ firstButton.addEventListener('click', function(){
 
 
 
-// function updateWeather() {
-//     get(information)
-//     .then(function(response){
-        
-//     }); 
-// }
+
 
 
