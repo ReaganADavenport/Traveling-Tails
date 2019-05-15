dbbf6d99bc59941bb9a07eb6a9e7daf23b471d29
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-05-15 09:24:37 -0400

    added changes I deleted and added addMap to the updateMap function

--- travelingTails.js
+++ travelingTails.js
@@ -14,11 +14,23 @@ function get(url) {
         });
 }
 
-function updateWeather() {
+function updateMap() {
     get(information)
     .then(function(response){
-        
+        addMap(response);   
     }); 
 }
 
+function addMap(object) {
+    // get lat and long coordinates
+    const zip = object;
+    // Use the Lat/Long to create a map
+    const mapUrl = `http://maps.google.com/maps?q=${zip}&output=embed`;
+    // create iframe and set attributes
+    const iframe = document.createElement('iframe');
+    iframe.src = mapUrl;
+    document.querySelector(".images").append(iframe);
+}
 
+
+updateMap;
