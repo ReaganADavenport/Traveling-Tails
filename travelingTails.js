4325cb9d04df1607055ad3c265b6db6144068b66
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-15 12:05:48 -0400

    Made button link to second page

--- travelingTails.js
+++ travelingTails.js
@@ -1,27 +1,56 @@
 "use strict"
 
-let hotelInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=hotels-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+// let hotelInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=hotels-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-let parksInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+// let parksInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-let storeInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+// let storeInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-let vetInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+// let vetInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-function get(url) {
-    return fetch(url)
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(data) {
-            return data;
-        })
-        .catch(function(error) {
-            return error;
-        });
+let firstButton =document.querySelector('#first-page-button');
+
+function visitPage(){
+    window.location='tTails-page2.html';
 }
 
-new google.maps.places.Autocomplete(document.getElementById('#search-bar'));
+firstButton.addEventListener('click', function(){
+    console.log('hi');
+    visitPage();
+});
+
+
+
+
+
+
+
+// function get(url) {
+//     return fetch(url)
+//         .then(function(response) {
+//             return response.json()
+//         })
+//         .then(function(data) {
+//             return data;
+//         })
+//         .catch(function(error) {
+//             return error;
+//         });
+// }
+
+
+
+
+
+
+
+// function updateSearch() {
+//     get()
+//     .then(function(response){
+//         addLocationName(response);
+// }
+
+
 
 
 
