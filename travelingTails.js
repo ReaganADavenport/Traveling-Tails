2f01f813da28356b4db91049b349d5ba3e09e9fc
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-16 07:56:00 -0400

    Fixed some input issues, made toggle function

--- travelingTails.js
+++ travelingTails.js
@@ -1,18 +1,18 @@
 "use strict"
 
 let firstButton =document.querySelector('#first-page-button');
-let locationInput = document.querySelector('#search-bar');
+let locationInput =document.querySelector('#search-bar');
 // let locationInput = locationInput1.innerHTML;
 const locationValue = locationInput.value;
 // console.log("location", locationValue);
 
 const hotelInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=hotels-in-${locationValue}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-const parksInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+const parksInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=parks-in-${locationValue}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-const storeInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+const storeInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=stores-in-${locationValue}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
-const vetInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+const vetInfo= `https://maps.googleapis.com/maps/api/place/textsearch/xml?query=vets-in-${locationValue}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
 
 //pg2
 function get(url) {
@@ -100,14 +100,42 @@ function updateMap() {
 }
 //pg1
 
-function visitPage(){
-    window.location='tTails-page2.html';
-}
+// function visitPage(){
+//     window.location='tTails-page2.html';
+// }
+const secondPage = document.querySelector('#second-page');
+const firstPage = document.querySelector('#inner-body-wrapper');
 
-firstButton.addEventListener('click', function(){
-    visitPage();
-    // updateMap();
-    console.log(store.result.geometry.location.lat);
+// function show(element) {
+//     element.style.display = value;
+// };
+
+// function hide(element) {
+//     element.style.display = "none";
+// };
+
+// function toggle(element) {
+//     if (element.style.display === 'none') {
+//         show(element);
+//         return;
+//     } else {
+//         hide(element);
+//     }
+// };
+
+
+firstButton.addEventListener('click', function(e){
+    e.preventDefault;
+    // toggle(secondPage);
+    // console.log(hotel.result.geometry.location.lat);
+    if (secondPage.style.display === 'none') {
+        secondPage.style.display = 'block';
+        firstPage.style.display = 'none';
+    } else {
+        secondPage.style.display = 'none';
+        firstPage.style.display = 'block';
+    }
+    updateMap();    
 });
 
 
