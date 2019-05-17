4d69a8a7b1542ad929bc6dcc9184fbf02b49e1d6
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-17 10:07:17 -0400

    Added other locations in

--- travelingTails.js
+++ travelingTails.js
@@ -1,6 +1,11 @@
 "use strict"
 
 const firstButton =document.querySelector('#first-page-button');
+const hotelsButton =document.querySelector('#hotels-button');
+const parksButton =document.querySelector('#parks-button');
+const vetsButton =document.querySelector('#vets-button');
+const storesButton =document.querySelector('#stores-button');
+
 const secondPage = document.querySelector('#second-page');
 const firstPage = document.querySelector('#inner-body-wrapper');
 // const locationInput = document.querySelector('#search-bar');
@@ -9,7 +14,7 @@ const firstPage = document.querySelector('#inner-body-wrapper');
 
 firstButton.addEventListener('click', function(e){
     e.preventDefault;
-    setLocations();
+    setHotelLocations();
     // toggle(secondPage);
     if (secondPage.style.display === 'none') {
         secondPage.style.display = 'block';
@@ -19,6 +24,28 @@ firstButton.addEventListener('click', function(e){
         firstPage.style.display = 'block';
     }    
 });
+
+
+parksButton.addEventListener('click', function(e) {
+    e.preventDefault;
+    setParkLocations();
+});
+
+hotelsButton.addEventListener('click', function(e) {
+    e.preventDefault;
+    setHotelLocations();
+});
+
+vetsButton.addEventListener('click', function(e) {
+    e.preventDefault;
+    setVetLocations();
+});
+
+storesButton.addEventListener('click', function(e) {
+    e.preventDefault;
+    setStoreLocations();
+});
+
 
 function get(url) {
     return fetch(url)
@@ -33,7 +60,7 @@ function get(url) {
         });
 };
 
-function setLocations() {
+function setHotelLocations() {
     // Grabbing the user input
     const locationInput = document.querySelector('#search-bar').value;
 
@@ -63,6 +90,97 @@ function setLocations() {
 
 };
 
+function setParkLocations() {
+    // Grabbing the user input
+    const locationInput = document.querySelector('#search-bar').value;
+
+    // Grabbing the API library for parks and inputing the user input
+    let parksInfo= `https://my-little-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=parks-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+
+    get(parksInfo)
+    .then(function(response) {
+        const results = response.results;
+        let latArray = [];
+        let longArray = [];
+        results.forEach((result) => {
+            // console.log("result is", result);
+            const lat = Number(result.geometry.location.lat);
+            const long = Number(result.geometry.location.lng);
+            latArray.push(lat);
+            longArray.push(long);
+        });
+    return [latArray, longArray];
+    })
+
+    .then(function(coordArray) {
+        const latArray = coordArray[0];
+        const longArray = coordArray[1];
+        markPlaces(latArray,longArray, latArray,longArray);
+        });
+
+};
+
+
+function setVetLocations() {
+    // Grabbing the user input
+    const locationInput = document.querySelector('#search-bar').value;
+
+    // Grabbing the API library for parks and inputing the user input
+    let vetsInfo= `https://my-little-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=vets-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+
+    get(vetsInfo)
+    .then(function(response) {
+        const results = response.results;
+        let latArray = [];
+        let longArray = [];
+        results.forEach((result) => {
+            // console.log("result is", result);
+            const lat = Number(result.geometry.location.lat);
+            const long = Number(result.geometry.location.lng);
+            latArray.push(lat);
+            longArray.push(long);
+        });
+    return [latArray, longArray];
+    })
+
+    .then(function(coordArray) {
+        const latArray = coordArray[0];
+        const longArray = coordArray[1];
+        markPlaces(latArray,longArray, latArray,longArray);
+        });
+
+};
+
+
+function setStoreLocations() {
+    // Grabbing the user input
+    const locationInput = document.querySelector('#search-bar').value;
+
+    // Grabbing the API library for parks and inputing the user input
+    let storesInfo= `https://my-little-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=stores-in-${locationInput}&key=AIzaSyBdsm65ywFiu-1TK-v03CKyD03g3T4i0AA&type=other_pet_services`;
+
+    get(storesInfo)
+    .then(function(response) {
+        const results = response.results;
+        let latArray = [];
+        let longArray = [];
+        results.forEach((result) => {
+            // console.log("result is", result);
+            const lat = Number(result.geometry.location.lat);
+            const long = Number(result.geometry.location.lng);
+            latArray.push(lat);
+            longArray.push(long);
+        });
+    return [latArray, longArray];
+    })
+
+    .then(function(coordArray) {
+        const latArray = coordArray[0];
+        const longArray = coordArray[1];
+        markPlaces(latArray,longArray, latArray,longArray);
+        });
+
+};
 //Making the map, setting it to the first coordinates in the location inputed Array
 function markPlaces(latitude, longitude, inDesLat, inDesLong) {
     const options = {
@@ -79,23 +197,6 @@ function markPlaces(latitude, longitude, inDesLat, inDesLong) {
         });
     }
 }
-
-
-
-// function markHotels(object){
-//     object.forEach(function(hotel) {
-//         const lat = hotel.PlaceSearchResponse.geometry.location.lat;
-//         const lng = hotel.PlaceSearchResponse.geometry.location.lng;
-
-//         new google.maps.Marker({
-//             position:{lat, lng},
-//             map: map
-//         });
-//     });
-// }
-
-
-
 
 
 
