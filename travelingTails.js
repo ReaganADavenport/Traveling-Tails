8616e7e7576fdba1719af2f9725b5d6853f93fab
Liz Aitken <liz.a.aitken@gmail.com>
2019-05-20 16:20:27 -0400

    Fixed git stuff- now it is all combined

--- travelingTails.js
+++ travelingTails.js
@@ -12,8 +12,6 @@ const secondPage = document.querySelector('#second-page');
 const firstPage = document.querySelector('#inner-body-wrapper');
 const dogImage = document.querySelector('.image-dog2');
 
-
-
 //Bending the title
 //Credit to: https://appendto.com/2016/09/how-to-make-circularcurved-text-with-javascript/
 circularText("                 Traveling Tails", 230, 0);
@@ -36,7 +34,7 @@ firstButton.addEventListener('click', function(e){
     //Checks to see if the search-bar is empty
     if (document.querySelector('#search-bar').value === ''){
         //if it is send an alert asking the user for a zip code then reset the page
-        alert('Enter a valid location');
+        alert('Enter a zip code');
         secondPage.style.display = 'block';
         firstPage.style.display = 'none';
     } else {
@@ -342,13 +340,11 @@ function setRestaurantLocations() {
 };
 //Making the map, setting it to the first coordinates in the location inputed Array
 function markPlaces(name, rating, latitude, longitude, inDesLat, inDesLong, iconPix) {
-    // console.log(data.status);
     const options = {
         zoom: 11,
         //took out [0]s 
         center: {lat:parseFloat(inDesLat[0]),lng:parseFloat(inDesLong[0])},
     }
-    
     let map = new google.maps.Map(document.getElementById('map'), options);
     //Places a marker on each location
     for (let i = 0; i <= latitude.length; i++) {
@@ -374,7 +370,7 @@ function markPlaces(name, rating, latitude, longitude, inDesLat, inDesLong, icon
             });
 
         } else {
-            let infoWin = new google.maps.InfoWindow({
+            let infoWindow = new google.maps.InfoWindow({
                 // content: url+ "<h2>This place has no rating yet!</h2>"
                 content: url+ `<h2>${rating[i]}</h2>`
             });
@@ -387,5 +383,9 @@ function markPlaces(name, rating, latitude, longitude, inDesLat, inDesLong, icon
             });
         }
 
+        // marker.addListener('click', function(){
+        //     infoWindow.open(map, marker);
+        
+        // })
     }
 }
